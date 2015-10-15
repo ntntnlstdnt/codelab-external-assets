eb582f4a1adb518925c8a7ebd605989cb73dc938
International <international.resource.help@gmail.com>
2015-10-14 20:08:10 -0400

    External files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var name = prompt('What is thy name?');
+if (name !== null){
+  document.getElementById('name').innerHTML =
+  "Hey my name is " + name + "." ;
+}
+
+var color = prompt('Have a favorite color?');
+if (color !== null){
+  document.getElementById('color').innerHTML =
+ "My favorite color is "+ color + "." ;
+}
+
+
+var sport = prompt('So what is your favorite sport?');
+if (sport !== null){
+  document.getElementById('sport').innerHTML =
+ "I like playing/watching "+ sport + "." ; }
